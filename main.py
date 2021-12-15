ca92cf852daaedfc6acb4ac0675754ac156cf66b
dminyaylo <90289519+dminyaylo@users.noreply.github.com>
2021-12-15 20:04:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,17 @@ def test_apple():
     browser = webdriver.Chrome(options=chrome_options)
     browser.get('https://www.apple.com/')
     browser.implicitly_wait(100)
+
+
+    clickAirPods = browser.find_element(By.XPATH, '//*[@id="ac-globalnav"]/div/ul[2]/li[7]/a').click()
+
+    clickAirPodsModel = browser.find_element(By.XPATH, '//*[@id="chapternav"]/div/ul/li[1]/a/span').click()
+
+    clickBuy = browser.find_element(By.XPATH, '//*[@id="ac-localnav"]/div/div[2]/div[2]/div[2]/div[2]/a').click()
+
+    result = browser.find_element(By.XPATH, '//*[@id="root"]/div[3]/div[1]/div[1]/div/div[1]/div/span')
+
+    assert result.tag_name == 'span'
+    assert result.text == '$129.00'
+
+    browser.quit()
